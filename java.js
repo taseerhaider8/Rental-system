f63b04c4e358d6bf6cb6da689f337d7fadbe5c23
taseerhaider8 <taseerhaider841@gmail.com>
2026-02-28 14:37:09 +0500

    Laeder Taseer All Files

---
+++ java.js
@@ -0,0 +1,148 @@
+// Shop page
+function myfrock(){
+    // product images
+    document.getElementById('product1-image').innerHTML='<img src="./Images/Product/frocks/f1.jpg" alt="" class="image-fluid">';
+    document.getElementById('product2-image').innerHTML='<img src="./Images/Product/frocks/f2.jpg" alt="" class="image-fluid">';
+    document.getElementById('product3-image').innerHTML='<img src="./Images/Product/frocks/f3.jpg" alt="" class="image-fluid">';
+    document.getElementById('product4-image').innerHTML='<img src="./Images/Product/frocks/f4.jpg" alt="" class="image-fluid">';
+    document.getElementById('product5-image').innerHTML='<img src="./Images/Product/frocks/f5.jpg" alt="" class="image-fluid">';
+    document.getElementById('product6-image').innerHTML='<img src="./Images/Product/frocks/f6.jpg" alt="" class="image-fluid">';
+    document.getElementById('product7-image').innerHTML='<img src="./Images/Product/frocks/f7.jpg" alt="" class="image-fluid">';
+    document.getElementById('product8-image').innerHTML='<img src="./Images/Product/frocks/f8.jpg" alt="" class="image-fluid">';
+    document.getElementById('product9-image').innerHTML='<img src="./Images/Product/frocks/f9.jpg" alt="" class="image-fluid">';
+    //product name
+    document.getElementById('product1-name').innerHTML='New fancy Frock';
+    document.getElementById('product2-name').innerHTML='New fancy Frock';
+    document.getElementById('product3-name').innerHTML='New fancy Frock';
+    document.getElementById('product4-name').innerHTML='New fancy Frock';
+    document.getElementById('product5-name').innerHTML='New fancy Frock';
+    document.getElementById('product6-name').innerHTML='New fancy Frock';
+    document.getElementById('product7-name').innerHTML='New fancy Frock';
+    document.getElementById('product8-name').innerHTML='New fancy Frock';
+    document.getElementById('product9-name').innerHTML='New fancy Frock';
+}
+function myglases(){
+    // product images
+    document.getElementById('product1-image').innerHTML='<img src="./Images/Product/Glasses/g1.jpg" alt="" class="image-fluid">';
+    document.getElementById('product2-image').innerHTML='<img src="./Images/Product/Glasses/g2.jpg" alt="" class="image-fluid">';
+    document.getElementById('product3-image').innerHTML='<img src="./Images/Product/Glasses/g3.jpg" alt="" class="image-fluid">';
+    document.getElementById('product4-image').innerHTML='<img src="./Images/Product/Glasses/g4.jpg" alt="" class="image-fluid">';
+    document.getElementById('product5-image').innerHTML='<img src="./Images/Product/Glasses/g5.jpg" alt="" class="image-fluid">';
+    document.getElementById('product6-image').innerHTML='<img src="./Images/Product/Glasses/g6.jpg" alt="" class="image-fluid">';
+    document.getElementById('product7-image').innerHTML='<img src="./Images/Product/Glasses/g7.jpg" alt="" class="image-fluid">';
+    document.getElementById('product8-image').innerHTML='<img src="./Images/Product/Glasses/g8.jpg" alt="" class="image-fluid">';
+    document.getElementById('product9-image').innerHTML='<img src="./Images/Product/Glasses/g9.jpg" alt="" class="image-fluid">';
+    //product name
+    document.getElementById('product2-name').innerHTML='New Glases Designs';
+    document.getElementById('product3-name').innerHTML='New Glases Designs';
+    document.getElementById('product1-name').innerHTML='New Glases Designs';
+    document.getElementById('product4-name').innerHTML='New Glases Designs';
+    document.getElementById('product5-name').innerHTML='New Glases Designs';
+    document.getElementById('product6-name').innerHTML='New Glases Designs';
+    document.getElementById('product7-name').innerHTML='New Glases Designs';
+    document.getElementById('product8-name').innerHTML='New Glases Designs';
+    document.getElementById('product9-name').innerHTML='New Glases Designs';
+}
+function mywatch(){
+    // product images
+    document.getElementById('product1-image').innerHTML='<img src="./Images/Product/Watch/w1.jpg" alt="" class="image-fluid" >'
+    document.getElementById('product2-image').innerHTML='<img src="./Images/Product/Watch/w2.jpg" alt="" class="image-fluid" >'
+    document.getElementById('product3-image').innerHTML='<img src="./Images/Product/Watch/w3.png" alt="" class="image-fluid" >'
+    document.getElementById('product4-image').innerHTML='<img src="./Images/Product/Watch/w4.jpg" alt="" class="image-fluid" >'
+    document.getElementById('product5-image').innerHTML='<img src="./Images/Product/Watch/w5.jpg" alt="" class="image-fluid" >'
+    document.getElementById('product6-image').innerHTML='<img src="./Images/Product/Watch/w6.jpg" alt="" class="image-fluid" >'
+    document.getElementById('product7-image').innerHTML='<img src="./Images/Product/Watch/w7.jpg" alt="" class="image-fluid" >'
+    document.getElementById('product8-image').innerHTML='<img src="./Images/Product/Watch/w8.jpg" alt="" class="image-fluid" >'
+    document.getElementById('product9-image').innerHTML='<img src="./Images/Product/Watch/w9.jpg" alt="" class="image-fluid" >'
+    //product name
+    document.getElementById('product2-name').innerHTML='New Watch Designs';
+    document.getElementById('product3-name').innerHTML='New Watch Designs';
+    document.getElementById('product1-name').innerHTML='New Watch Designs';
+    document.getElementById('product4-name').innerHTML='New Watch Designs';
+    document.getElementById('product5-name').innerHTML='New Watch Designs';
+    document.getElementById('product6-name').innerHTML='New Watch Designs';
+    document.getElementById('product7-name').innerHTML='New Watch Designs';
+    document.getElementById('product8-name').innerHTML='New Watch Designs';
+    document.getElementById('product9-name').innerHTML='New Watch Designs';
+}
+function mypurse(){
+    // product images
+    document.getElementById('product1-image').innerHTML='<img src="./Images/Product/purse bags/p1.jpg" alt="" class="image-fluid">';
+    document.getElementById('product2-image').innerHTML='<img src="./Images/Product/purse bags/p2.jpg" alt="" class="image-fluid">';
+    document.getElementById('product3-image').innerHTML='<img src="./Images/Product/purse bags/p3.jpg" alt="" class="image-fluid">';
+    document.getElementById('product4-image').innerHTML='<img src="./Images/Product/purse bags/p4.jpg" alt="" class="image-fluid">';
+    document.getElementById('product5-image').innerHTML='<img src="./Images/Product/purse bags/p5.jpg" alt="" class="image-fluid">';
+    document.getElementById('product6-image').innerHTML='<img src="./Images/Product/purse bags/p6.jpg" alt="" class="image-fluid">';
+    document.getElementById('product7-image').innerHTML='<img src="./Images/Product/purse bags/p7.jpg" alt="" class="image-fluid">';
+    document.getElementById('product8-image').innerHTML='<img src="./Images/Product/purse bags/p8.jpg" alt="" class="image-fluid">';
+    document.getElementById('product9-image').innerHTML='<img src="./Images/Product/purse bags/p9.jpg" alt="" class="image-fluid">';
+    //product name
+    document.getElementById('product2-name').innerHTML='New Handbags Designs';
+    document.getElementById('product3-name').innerHTML='New Handbags Designs';
+    document.getElementById('product1-name').innerHTML='New Handbags Designs';
+    document.getElementById('product4-name').innerHTML='New Handbags Designs';
+    document.getElementById('product5-name').innerHTML='New Handbags Designs';
+    document.getElementById('product6-name').innerHTML='New Handbags Designs';
+    document.getElementById('product7-name').innerHTML='New Handbags Designs';
+    document.getElementById('product8-name').innerHTML='New Handbags Designs';
+    document.getElementById('product9-name').innerHTML='New Handbags Designs';
+}
+
+//Shale Page area
+
+function sale_myfrock(){
+    // product images
+    document.getElementById('product1-image').innerHTML='<img src="./Images/Product/frocks/f7.jpg" alt="" class="image-fluid">';
+    document.getElementById('product2-image').innerHTML='<img src="./Images/Product/frocks/f4.jpg" alt="" class="image-fluid">';
+    document.getElementById('product3-image').innerHTML='<img src="./Images/Product/frocks/f9.jpg" alt="" class="image-fluid">';
+    document.getElementById('product4-image').innerHTML='<img src="./Images/Product/frocks/f5.jpg" alt="" class="image-fluid">';
+    document.getElementById('product5-image').innerHTML='<img src="./Images/Product/frocks/f2.jpg" alt="" class="image-fluid">';
+    //product name
+    document.getElementById('product1-name').innerHTML='New fancy Frock';
+    document.getElementById('product2-name').innerHTML='New fancy Frock';
+    document.getElementById('product3-name').innerHTML='New fancy Frock';
+    document.getElementById('product4-name').innerHTML='New fancy Frock';
+    document.getElementById('product5-name').innerHTML='New fancy Frock';
+}
+function sale_myglases(){
+    // product images
+    document.getElementById('product1-image').innerHTML='<img src="./Images/Product/Glasses/g4.jpg" alt="" class="image-fluid">';
+    document.getElementById('product2-image').innerHTML='<img src="./Images/Product/Glasses/g7.jpg" alt="" class="image-fluid">';
+    document.getElementById('product3-image').innerHTML='<img src="./Images/Product/Glasses/g9.jpg" alt="" class="image-fluid">';
+    document.getElementById('product4-image').innerHTML='<img src="./Images/Product/Glasses/g6.jpg" alt="" class="image-fluid">';
+    document.getElementById('product5-image').innerHTML='<img src="./Images/Product/Glasses/g1.jpg" alt="" class="image-fluid">';
+    //product name
+    document.getElementById('product2-name').innerHTML='New Glases Designs';
+    document.getElementById('product3-name').innerHTML='New Glases Designs';
+    document.getElementById('product1-name').innerHTML='New Glases Designs';
+    document.getElementById('product4-name').innerHTML='New Glases Designs';
+    document.getElementById('product5-name').innerHTML='New Glases Designs';
+}
+function sale_mywatch(){
+    // product images
+    document.getElementById('product1-image').innerHTML='<img src="./Images/Product/Watch/w7.jpg" alt="" class="image-fluid" >'
+    document.getElementById('product2-image').innerHTML='<img src="./Images/Product/Watch/w9.jpg" alt="" class="image-fluid" >'
+    document.getElementById('product3-image').innerHTML='<img src="./Images/Product/Watch/w4.jpg" alt="" class="image-fluid" >'
+    document.getElementById('product4-image').innerHTML='<img src="./Images/Product/Watch/w6.jpg" alt="" class="image-fluid" >'
+    document.getElementById('product5-image').innerHTML='<img src="./Images/Product/Watch/w2.jpg" alt="" class="image-fluid" >'
+    //product name
+    document.getElementById('product2-name').innerHTML='New Watch Designs';
+    document.getElementById('product3-name').innerHTML='New Watch Designs';
+    document.getElementById('product1-name').innerHTML='New Watch Designs';
+    document.getElementById('product4-name').innerHTML='New Watch Designs';
+    document.getElementById('product5-name').innerHTML='New Watch Designs';
+}
+function sale_mypurse(){
+    // product images
+    document.getElementById('product1-image').innerHTML='<img src="./Images/Product/purse bags/p9.jpg" alt="" class="image-fluid">';
+    document.getElementById('product2-image').innerHTML='<img src="./Images/Product/purse bags/p7.jpg" alt="" class="image-fluid">';
+    document.getElementById('product3-image').innerHTML='<img src="./Images/Product/purse bags/p6.jpg" alt="" class="image-fluid">';
+    document.getElementById('product4-image').innerHTML='<img src="./Images/Product/purse bags/p3.jpg" alt="" class="image-fluid">';
+    document.getElementById('product5-image').innerHTML='<img src="./Images/Product/purse bags/p1.jpg" alt="" class="image-fluid">';
+    //product name
+    document.getElementById('product2-name').innerHTML='New Handbags Designs';
+    document.getElementById('product3-name').innerHTML='New Handbags Designs';
+    document.getElementById('product1-name').innerHTML='New Handbags Designs';
+    document.getElementById('product4-name').innerHTML='New Handbags Designs';
+    document.getElementById('product5-name').innerHTML='New Handbags Designs';
+}
